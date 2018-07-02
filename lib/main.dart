f0c0a352afa83805a26097716386d239910e8840
Johannes <johanneshage97@gmail.com>
2018-07-02 12:01:10 +0200

    stopped at video 72

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
-import './product_manager.dart';
+import './pages/home.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowMaterialGrid: true,
       theme: ThemeData(
+          brightness: Brightness.light,
           primarySwatch: Colors.deepOrange,
-          accentColor: Colors.deepPurple,
-          brightness: Brightness.light),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: ProductManager('Food Tester'),
-      ),
+          accentColor: Colors.deepPurple),
+      home: HomePage(),
     );
   }
 }
