035b1acedb4ab60ff4f3f7fbd898c08503581d21
Johannes <johanneshage97@gmail.com>
2018-07-07 18:00:37 +0200

    stopped at video 173

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:scoped_model/scoped_model.dart';
+
 import '../widgets/products/products.dart';
+import '../scoped-models/main.dart';
 
-class ProductsPage extends StatelessWidget {
-  final List<Map<String, dynamic>> products;
+class ProductsPage extends StatefulWidget {
+  final MainModel model;
 
-  ProductsPage(this.products);
+  ProductsPage(this.model);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductsPageState();
+  }
+}
+
+class _ProductsPageState extends State<ProductsPage> {
+  @override
+  initState() {
+    widget.model.fetchProducts();
+    super.initState();
+  }
 
   Widget _buildSideDrawer(BuildContext context) {
     return Drawer(
@@ -27,6 +43,20 @@ class ProductsPage extends StatelessWidget {
     );
   }
 
+  Widget _buildProductsList() {
+    return ScopedModelDescendant(
+      builder: (BuildContext context, Widget child, MainModel model) {
+        Widget content = Center(child: Text('No Products Found!'));
+        if (model.displayedProducts.length > 0 && !model.isLoading) {
+          content = Products();
+        } else if (model.isLoading) {
+          content = Center(child: CircularProgressIndicator());
+        }
+        return content;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,13 +64,21 @@ class ProductsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('EasyList'),
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+          ScopedModelDescendant<MainModel>(
+            builder: (BuildContext context, Widget child, MainModel model) {
+              return IconButton(
+                icon: Icon(model.displayFavoritesOnly
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                onPressed: () {
+                  model.toggleDisplayMode();
+                },
+              );
+            },
           )
         ],
       ),
-      body: Products(products),
+      body: _buildProductsList(),
     );
   }
 }
